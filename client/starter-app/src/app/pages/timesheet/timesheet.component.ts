54452ded10767803e43af96db44ace8d1de60d6a
Christine John <christine.john@people10.com>
2017-08-05 14:55:31 +0530

    timesheet good mock data

--- client/starter-app/src/app/pages/timesheet/timesheet.component.ts
+++ client/starter-app/src/app/pages/timesheet/timesheet.component.ts
@@ -13,24 +13,27 @@ export class TimesheetComponent implements OnInit {
   }
 
   private timesheets =  [
-        {"project": "Alveo", "date": 2012, "status": "Pending", "activity": "dsad231ff"},
-        {"project": "Alveo", "date": 2011, "status": "Pending", "activity": "gwregre345"},
-        {"project": "Alveo", "date": 2005, "status": "Approved", "activity": "h354htr"},
-        {"project": "Allegiant", "date": 2003, "status": "Denied", "activity": "j6w54qgh"},
-        {"project": "Allegiant", "date": 1995, "status": "Pending", "activity": "hrtwy34"},
-        {"project": "Allegiant", "date": 2005, "status": "Pending", "activity": "jejtyj"},
-        {"project": "Allegiant", "date": 2012, "status": "Yellow", "activity": "g43gr"},
-        {"project": "P10 Internal", "date": 2013, "status": "Pending", "activity": "greg34"},
-        {"project": "P10 Internal", "date": 2000, "status": "Pending", "activity": "h54hw5"},
-        {"project": "P10 Internal", "date": 2013, "status": "Red", "activity": "245t2s"},
-        {"project": "Alveo", "date": 2005, "status": "Approved", "activity": "h354htr"},
-        {"project": "Allegiant", "date": 2003, "status": "Denied", "activity": "j6w54qgh"},
-        {"project": "Allegiant", "date": 1995, "status": "Pending", "activity": "hrtwy34"},
-        {"project": "Allegiant", "date": 2005, "status": "Pending", "activity": "jejtyj"},
-        {"project": "Allegiant", "date": 2012, "status": "Yellow", "activity": "g43gr"},
-        {"project": "P10 Internal", "date": 2013, "status": "Pending", "activity": "greg34"},
-        {"project": "P10 Internal", "date": 2000, "status": "Pending", "activity": "h54hw5"},
-        {"project": "P10 Internal", "date": 2013, "status": "Red", "activity": "245t2s"}
+      {"date":new Date(), "status":"Pending" ,"activities":[
+          {"duration":3,"activity":"Design","project":"P10 Internal"},
+          {"duration":2,"activity":"Development","project":"P10 Internal"},
+          {"duration":3,"activity":"Planing","project":"P10 Internal"},
+        ]},
+      {"date":new Date(), "status":"Pending" ,"activities":[
+          {"duration":3,"activity":"Design","project":"P10 Internal"},
+          {"duration":2,"activity":"Development","project":"P10 Internal"},
+          {"duration":3,"activity":"Planing","project":"P10 Internal"},
+        ]},
+      {"date":new Date(), "status":"Pending" ,"activities":[
+          {"duration":3,"activity":"Design","project":"P10 Internal"},
+          {"duration":2,"activity":"Development","project":"P10 Internal"},
+          {"duration":3,"activity":"Planing","project":"P10 Internal"},
+        ]},
+      {"date":new Date(), "status":"Pending" ,"activities":[
+          {"duration":3,"activity":"Design","project":"P10 Internal"},
+          {"duration":2,"activity":"Development","project":"P10 Internal"},
+          {"duration":3,"activity":"Planing","project":"P10 Internal"},
+        ]}
+
     ]
 
 }
