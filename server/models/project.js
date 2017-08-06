6778c9475d12a6bba9a9f8e221d2669206e15efd
vijay hudge <vijayhudge89@gmail.com>
2017-08-06 15:18:43 +0530

    Added comment for project model

--- server/models/project.js
+++ server/models/project.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 let Schema = mongoose.Schema;
 let Promise = require('bluebird');
 
+//TODO: team - should be objectId's of employees
 let ProjectSchema = new Schema({ projectId: String, name: String, manager: String, team: [] }, { collection: 'projects' });
 
 let Project = mongoose.model('Project', ProjectSchema);
